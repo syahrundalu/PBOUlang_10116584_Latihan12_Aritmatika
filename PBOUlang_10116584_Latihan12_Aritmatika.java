f2c46f0c587d0a476e8518ea35f22b6e1b7d952a
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:55:46 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan12_Aritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan12_Aritmatika;
+
+/**
+ *
+ * @author 
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan12_Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10, b = 20, c = 25, d = 25;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a x b = " + (a*b));
+        System.out.println("b : a = " + (b/a));
+        System.out.println("b % a = " + (b%a));
+        System.out.println("c % a = " + (c%a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("b--   = " + (b--));
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
+    }
+    
+}
